ac5e848885ba19632ea583666a883451a642eb8b
Hubert Jr <hubertjunior254@icloud.com>
2022-11-29 14:51:59 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const spy = ['Guadalupe', 'Ollie', 'Aki']
+function writeCards(spy){
+
+    const surprise = []
+    for (let i = 0; i<spy.length; i++ ){
+
+        surprise.push (`Thank you, ${spy[i]}, for the wonderful surprise gift!`)
+        console.log(surprise)
+
+    }
+
+    return surprise;
+}
+
+function countDown (){
+    for (let p = 10; p>=0; p--){
+        console.log(p)
+    }
+}
+
+countDown()
